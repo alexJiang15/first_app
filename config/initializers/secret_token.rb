246e91e115425c29126bbdbd7142f90b64cd81ff
Alex Jiang <dashuai99@gmail.com>
2013-12-13 23:05:03 -0500

    Initial commit: added files and changed .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'bd0355f673add7a88292ff3ac67c2a4e360bd903000b23a6668f02b901a48069d228387ff4311efb95fdf0ee722c58da60bf81f9cc4ea415c8a65093f6b27745'
